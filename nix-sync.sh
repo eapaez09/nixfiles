c64721e70dfb0408a051e7e72e94ed08bcfa8b44
edwin09pz <edwin09pz@duck.com>
2023-01-31 12:39:45 -0500

    NixOS update

--- nix-sync.sh
+++ nix-sync.sh
@@ -3,18 +3,38 @@ git pull
 
 cd
 
-# SPECIFIC APPS
-# alacritty
-rm -R $HOME/Documents/nixfiles/dotconfig/1080p/alacritty/
-cp -R $HOME/.config/alacritty/ $HOME/Documents/nixfiles/dotconfig/1080p/
+if [[ $1 == "hp" ]]; then
+    # SPECIFIC APPS
+    # alacritty
+    rm -R $HOME/Documents/nixfiles/dotconfig/1080p/alacritty/
+    cp -R $HOME/.config/alacritty/ $HOME/Documents/nixfiles/dotconfig/1080p/
 
-# i3
-rm -R $HOME/Documents/nixfiles/dotconfig/1080p/i3/
-cp -R $HOME/.config/i3/ $HOME/Documents/nixfiles/dotconfig/1080p/
+    # i3
+    rm -R $HOME/Documents/nixfiles/dotconfig/1080p/i3/
+    cp -R $HOME/.config/i3/ $HOME/Documents/nixfiles/dotconfig/1080p/
 
-# rofi
-rm -R $HOME/Documents/nixfiles/dotconfig/1080p/rofi/
-cp -R $HOME/.config/rofi/ $HOME/Documents/nixfiles/dotconfig/1080p/
+    # rofi
+    rm -R $HOME/Documents/nixfiles/dotconfig/1080p/rofi/
+    cp -R $HOME/.config/rofi/ $HOME/Documents/nixfiles/dotconfig/1080p/
+
+elif [[ $1 == "asus" ]]; then
+    # SPECIFIC APPS
+    # alacritty
+    rm -R $HOME/Documents/nixfiles/dotconfig/768p/alacritty/
+    cp -R $HOME/.config/alacritty/ $HOME/Documents/nixfiles/dotconfig/768p/
+
+    # i3
+    rm -R $HOME/Documents/nixfiles/dotconfig/768p/i3/
+    cp -R $HOME/.config/i3/ $HOME/Documents/nixfiles/dotconfig/768p/
+
+    # rofi
+    rm -R $HOME/Documents/nixfiles/dotconfig/768p/rofi/
+    cp -R $HOME/.config/rofi/ $HOME/Documents/nixfiles/dotconfig/768p/
+
+else
+    echo "Error, don't forget to specify the machine type"
+
+fi
 
 # GLOBAL APPS
 # picom
