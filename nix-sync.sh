a57b57631ca09d7cb41523172dfa75aee08f18fa
edwin09pz <edwin09pz@duck.com>
2023-02-02 20:56:45 -0500

    Added polybar config

--- nix-sync.sh
+++ nix-sync.sh
@@ -4,7 +4,9 @@ git pull
 cd
 
 if [[ $1 == "hp" ]]; then
+    #
     # SPECIFIC APPS
+    #
     # alacritty
     rm -R $HOME/Documents/nixfiles/dotconfig/1080p/alacritty/
     cp -R $HOME/.config/alacritty/ $HOME/Documents/nixfiles/dotconfig/1080p/
@@ -17,8 +19,14 @@ if [[ $1 == "hp" ]]; then
     rm -R $HOME/Documents/nixfiles/dotconfig/1080p/rofi/
     cp -R $HOME/.config/rofi/ $HOME/Documents/nixfiles/dotconfig/1080p/
 
+    # polybar
+    rm -R $HOME/Documents/nixfiles/dotconfig/1080p/polybar/
+    cp -R $HOME/.config/polybar/ $HOME/Documents/nixfiles/dotconfig/1080p/
+
 elif [[ $1 == "asus" ]]; then
+    #
     # SPECIFIC APPS
+    #
     # alacritty
     rm -R $HOME/Documents/nixfiles/dotconfig/768p/alacritty/
     cp -R $HOME/.config/alacritty/ $HOME/Documents/nixfiles/dotconfig/768p/
@@ -31,12 +39,19 @@ elif [[ $1 == "asus" ]]; then
     rm -R $HOME/Documents/nixfiles/dotconfig/768p/rofi/
     cp -R $HOME/.config/rofi/ $HOME/Documents/nixfiles/dotconfig/768p/
 
+    # polybar
+    rm -R $HOME/Documents/nixfiles/dotconfig/768p/polybar/
+    cp -R $HOME/.config/polybar/ $HOME/Documents/nixfiles/dotconfig/768p/
+
+
 else
     echo "Error, don't forget to specify the machine type"
 
 fi
 
+#
 # GLOBAL APPS
+#
 # picom
 rm -R $HOME/Documents/nixfiles/dotconfig/global/picom/
 cp -R $HOME/.config/picom/ $HOME/Documents/nixfiles/dotconfig/global/
@@ -55,7 +70,7 @@ sudo cp /etc/nixos/configuration.nix $HOME/Documents/nixfiles/nixos/
 
 cd $HOME/Documents/nixfiles/
 git add -A
-git commit -m "NixOS update"
+git commit -m "$2"
 git push
 
 cd
