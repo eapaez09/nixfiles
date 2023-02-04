9b76c54eb7421e1eb859db397abce8ad049e32e7
edwin09pz <edwin09pz@duck.com>
2023-02-03 22:53:01 -0500

    Added nix-configure.sh

---
+++ nix-configure.sh
@@ -0,0 +1,52 @@
+cd $HOME/Documents/nixfiles
+git pull
+
+cd
+if [[  $1 == "hp" ]]; then
+
+    # dotfiles stuff
+    rm -R $HOME/.config/alacritty/ $HOME/.config/polybar/ $HOME/.config/rofi/
+    rm -R $HOME/.config/fish/ $HOME/.config/i3/ $HOME/.config/neofetch $HOME/.config/picom/
+    rm $HOME/.xinitrc
+
+    cp -R $HOME/Documents/nixfiles/dotconfig/1080p/* $HOME/.config/
+    cp -R $HOME/Documents/nixfiles/dotconfig/global/fish/ $HOME/.config/
+    cp -R $HOME/Documents/nixfiles/dotconfig/global/i3/ $HOME/.config/
+    cp -R $HOME/Documents/nixfiles/dotconfig/global/neofetch/ $HOME/.config/
+    cp -R $HOME/Documents/nixfiles/dotconfig/global/picom/ $HOME/.config/
+    cp $HOME/Documents/nixfiles/global/.xinitrc $HOME/
+
+    # nix os settings
+    sudo rm /etc/nixos/configuration.nix
+    sudo rm /etc//nixos/packages.nix
+
+    sudo cp $HOME/Documents/nixfiles/nixos/configuration.nix /etc/nixos/
+    sudo cp $HOME/Documents/nixfiles/nixos/packages.nix /etc/nixos/
+
+elif [[  $1 == "asus" ]]; then
+
+    # dotfiles stuff
+    rm -r $home/.config/alacritty/ $home/.config/polybar/ $home/.config/rofi/
+    rm -R $HOME/.config/fish/ $HOME/.config/i3/ $HOME/.config/neofetch $HOME/.config/picom/
+    rm $HOME/.xinitrc
+
+    cp -R $HOME/Documents/nixfiles/dotconfig/768p/* $HOME/.config/
+    cp -R $HOME/Documents/nixfiles/dotconfig/global/fish/ $HOME/.config/
+    cp -R $HOME/Documents/nixfiles/dotconfig/global/i3/ $HOME/.config/
+    cp -R $HOME/Documents/nixfiles/dotconfig/global/neofetch/ $HOME/.config/
+    cp -R $HOME/Documents/nixfiles/dotconfig/global/picom/ $HOME/.config/
+    cp $HOME/Documents/nixfiles/global/.xinitrc $HOME/
+
+    # nix os settings
+    sudo rm /etc/nixos/configuration.nix
+    sudo rm /etc//nixos/packages.nix
+
+    sudo cp $HOME/Documents/nixfiles/nixos/configuration.nix /etc/nixos/
+    sudo cp $HOME/Documents/nixfiles/nixos/packages.nix /etc/nixos/
+
+else
+    echo "Please, don't forget to specify you hostname"
+    echo "hp = 1920x1080"
+    echo "asus = 1366x768"
+
+fi
