682e72ad2e09ac8f6d527eaa4fab610a0be72c5a
eapaez09 <eapaez09@duck.com>
2023-02-26 19:04:27 -0500

    added some specific configs

--- nixos/asusc423/specific.nix
+++ nixos/asusc423/specific.nix
@@ -5,10 +5,31 @@
 
 {
 
+  # Graphics
+  boot.initrd.kernelModules = [ "i915" ];
+
+  # Kernel Parameters
+  boot.consoleLogLevel = 0;
+  boot.kernelParams = [
+    "video=eDP-1:1366x768@60"
+    "quiet"
+    "udev.log_level=3"
+  ];
+
   environment.systemPackages = with pkgs; [
     sof-firmware
 
   ];
+
+  hardware.opengl = {
+    enable = true;
+    extraPackages = with pkgs; [
+      intel-media-driver # LIBVA_DRIVER_NAME=iHD
+      vaapiIntel         # LIBVA_DRIVER_NAME=i965 (older but works better for Firefox/Chromium)
+      vaapiVdpau
+      libvdpau-va-gl
+    ];
+  };
 
   boot.extraModprobeConfig = ''
   options snd-intel-dspcfg dsp_driver=3
